3f0108967a4804dd9ef5fc8a965961a9cc710465
Jonathan Allen <jallen@snow.edu>
2023-09-19 08:23:27 -0600

    move project to subfolder

---
+++ ApiTests/Class1.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace ApiTests;
+
+[TestFixture]
+public class Class1
+{
+    [Test]
+    public void Pass()
+    {
+        Assert.Fail("What?!");
+    }
+
+    [Test]
+    public void Pass2() { Assert.Pass("Hooray!"); }
+}
